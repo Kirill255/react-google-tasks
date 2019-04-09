aef56693fac2e12e673e2292b05f4e0f473ea33d
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-04-09 14:38:33 +0300

    add react-google-login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    isAuthenticated: false,
+    user: null
+  };
+
+  onSignIn = (googleUser) => {
+    console.log("onSignIn!!!");
+    const user = googleUser && googleUser;
+    console.log(user);
+    this.setState({ isAuthenticated: true, user });
+  };
+
+  onSignInFailure = (error) => {
+    console.log("onSignInFailure!!!");
+    console.log(error);
+  };
+
+  onSignOut = () => {
+    this.setState({ isAuthenticated: false, user: null });
+  };
+
   render() {
+    console.log(this.state.user && this.state.user.isSignedIn());
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {!this.state.isAuthenticated ? (
+          <div className="signin__page">
+            <h1>Your Google Tasks!</h1>
+            <div className="signin__form">
+              <h2>Please, Login In!</h2>
+              <GoogleLogin
+                clientId={
+                  "407265720120-ks2il9jao2ts7320nufeg23u6s67b1oe.apps.googleusercontent.com"
+                }
+                buttonText="Login"
+                onSuccess={this.onSignIn}
+                onFailure={this.onSignInFailure}
+                cookiePolicy={"single_host_origin"}
+                isSignedIn={true}
+              />
+            </div>
+          </div>
+        ) : (
+          <GoogleLogout
+            clientId={"407265720120-ks2il9jao2ts7320nufeg23u6s67b1oe.apps.googleusercontent.com"}
+            buttonText="Logout"
+            onLogoutSuccess={this.onSignOut}
+          />
+        )}
       </div>
     );
   }
