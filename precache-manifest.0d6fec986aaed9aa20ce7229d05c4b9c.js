77c82c2cdf3e1cefd0386979423a4599ff0e6cfd
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-04-21 15:51:29 +0300

    Updates

---
+++ precache-manifest.0d6fec986aaed9aa20ce7229d05c4b9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffb6cc2c8c7b979828d4",
+    "url": "/Kirill255/react-google-tasks/static/css/main.5ee34161.chunk.css"
+  },
+  {
+    "revision": "ffb6cc2c8c7b979828d4",
+    "url": "/Kirill255/react-google-tasks/static/js/main.0c185290.chunk.js"
+  },
+  {
+    "revision": "0e4a576da6cffd4b936a",
+    "url": "/Kirill255/react-google-tasks/static/js/runtime~main.d1e40431.js"
+  },
+  {
+    "revision": "278639f025b986f3da34",
+    "url": "/Kirill255/react-google-tasks/static/js/2.7e96e397.chunk.js"
+  },
+  {
+    "revision": "a506c8831556284249aab17a5c4183a8",
+    "url": "/Kirill255/react-google-tasks/index.html"
+  }
+];
